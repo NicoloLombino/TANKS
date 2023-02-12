09f4f9c6cc0c6d033b12fbb74d131993b2fc67b0
Nicolò Lombino <lombino97@gmail.com>
2023-02-12 19:05:33 +0100

    improve menu system and add a new map (arena)

--- TANK/Assets/Nicolò/Scripts/GameController.cs
+++ TANK/Assets/Nicolò/Scripts/GameController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,25 +9,36 @@ public class GameController : MonoBehaviour
     
     public bool isGame;
 
-
-
+    public GameObject blackPanel;
+    public float timer;
+    public float time;
     
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (blackPanel.activeInHierarchy && isGame)
+        {
+            if(Time.time >= timer)
+            {
+                blackPanel.SetActive(false);
+            }
+        }
     }
+
 
     public void StartGame()
     {
         isGame = true;
+        blackPanel.SetActive(true);
+        timer = Time.time + time;
     }
+
 }
